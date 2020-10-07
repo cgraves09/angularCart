f34800562848819e9acea72581f643a39a9fcead
cgraves09 <christopher.graves09@gmail.com>
2020-10-07 15:03:44 -0400

    h1 font change for empty shopping cart div

--- src/app/components/checkout/checkout.component.ts
+++ src/app/components/checkout/checkout.component.ts
@@ -1,17 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 import { ProductService } from '../../services/products.service';
+import { Product } from '../../models/product';
 @Component({
   selector: 'app-checkout',
   templateUrl: './checkout.component.html',
   styleUrls: ['./checkout.component.css'],
 })
 export class CheckoutComponent implements OnInit {
+  productsList: Product[] = [];
   constructor(private productService: ProductService) {}
   total = 0;
   valid = true;
   success = false;
-  productNam = '';
-  productQty = 0;
+
   ranNum = Math.floor(Math.random() * 1000000000);
   ngOnInit(): void {
     this.productsList = this.productService.getProducts();
